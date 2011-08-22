e8dd0c2a4d5d736dd772ba899aba7af3f862204c
Marc MacLeod <marbemac@gmail.com>
2011-08-22 14:36:18 -0400

    Initial Commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectLimelight::Application.config.secret_token = 'fd8cc92e3e2f5128e76af9aa050691aeea7a490313573c5335cfbeed550a4f7436c8a2120e66caf86045997d18d8117dfa3294266b4fdffb0fb7708b007e643d'

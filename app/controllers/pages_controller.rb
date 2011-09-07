0c4ae52f6a2cf5a449c8b0c40c87a064328744ea
Marc MacLeod <marbemac@gmail.com>
2011-09-07 17:43:27 -0400

    gravatar testing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,16 @@ class PagesController < ApplicationController
     @title = 'Home'
     @core_objects = CoreObject.feed(session[:feed_filters][:display], [:created_at, :desc], {})
 
+    #gravatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.new.update(current_user.email)}?s=512&d=identicon"
+    #
+    #uploader = ImageUploader.new
+    #uploader.store! gravatar_url
+
+    #
+    #image = ImageUpload.upload
+    #image.upload(gravatar_image)
+    #upload.upload_file
+
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @core_objects }

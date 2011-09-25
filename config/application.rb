cec659357867eefd7c56940c1397e59a09358c38
Marc MacLeod <marbemac@gmail.com>
2011-09-25 15:49:33 -0400

    added timezone support
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,8 @@ module ProjectLimelight
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
+    config.time_zone = "Eastern Time (US & Canada)"
+
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password, :password_confirmation]
 

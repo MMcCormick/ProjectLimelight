aeb80f608739abf2a77d2c907258c20a2aa0a056
Marc MacLeod <marbemac@gmail.com>
2011-09-26 16:10:17 -0400

    update for testing
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- Gemfile
+++ Gemfile
@@ -5,17 +5,16 @@ source 'http://rubygems.org'
 gem 'rails', '3.1.0'
 gem 'execjs'
 gem 'jquery-rails'
+gem 'bson_ext'
 gem 'mongoid' # MongoDB
 gem 'mongoid_slug' # Automatic MongoDB slugs
 gem 'mongoid_auto_inc' # Auto incrementing fields in mongoid
 gem 'devise' # Authentication
 gem 'frontend-helpers'
 gem 'cells' # Components
-gem 'rspec-cells'
 gem 'yajl-ruby' # json processing
 gem 'redcarpet' # Markdown
 gem 'fog' # Cloud support (amazon s3, etc)
-gem 'rmagick' # Image manipulation
 gem 'carrierwave' # File uploads
 gem 'carrierwave-mongoid', :require => 'carrierwave/mongoid'
 gem 'embedly'
@@ -30,51 +29,45 @@ group :assets do
   gem 'uglifier'
 end
 
-gem "rspec-rails", ">= 2.6.1", :group => [:development, :test]
-
 group :development do
-  gem "rails-footnotes"
   gem "pry"
+  gem 'rspec-rails'
+  gem 'rspec-cells'
+  gem 'guard'
+  gem 'guard-rspec'
+  gem "rails-footnotes"
   gem "ruby-debug19"
   gem "foreman"
-  gem "guard"
-  gem "guard-bundler", ">= 0.1.3"
-  gem "guard-rails", ">= 0.0.3"
-  gem "guard-livereload"
-  gem "guard-rspec"
-  gem "guard-cucumber"
-  gem "spork"
-  gem "guard-spork"
-  gem 'bson_ext'
 
-  case HOST_OS
-    when /darwin/i
-      gem 'rb-fsevent'
-      gem 'growl'
-    when /linux/i
-      gem 'libnotify'
-      gem 'rb-inotify'
-    when /mswin|windows/i
-      gem 'rb-fchange'
-      gem 'win32console'
-      gem 'rb-notifu'
-  end
+  #case HOST_OS
+  #  when /darwin/i
+  #    gem 'rb-fsevent'
+  #    gem 'growl'
+  #  when /linux/i
+  #    gem 'libnotify'
+  #    gem 'rb-inotify'
+  #  when /mswin|windows/i
+  #    gem 'rb-fchange'
+  #    gem 'win32console'
+  #    gem 'rb-notifu'
+  #end
 
 end
 
 group :test do
-  gem "database_cleaner", ">= 0.6.7"
-  gem "mongoid-rspec", ">= 1.4.4"
-  gem "factory_girl_rails", ">= 1.1.0"
-  gem "cucumber-rails", ">= 1.0.2"
-  gem "capybara", ">= 1.0.0"
-  gem "launchy", ">= 2.0.5"
-  gem "ZenTest"
-  gem "autotest-rails-pure"
-  gem "autotest-growl"
-  gem "autotest-fsevent"
+  gem 'rspec'
+  gem "capybara"
+  gem "factory_girl_rails"
+  gem 'growl'
+  gem 'rb-fsevent'
+  gem "database_cleaner"
+  gem "mongoid-rspec"
+  gem "spork", "> 0.9.0.rc"
+  gem 'guard-spork'
 end
 
 if HOST_OS =~ /linux/i
-  gem 'therubyracer', '>= 0.8.2'
+  gem 'therubyracer'
 end
+
+gem 'rmagick', :require => false # Image manipulation (put rmagick at the bottom because it's a little bitch about everything)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,21 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'rubygems'
 require 'spork'
 
 Spork.prefork do
-  # Loading more in this block will cause your tests to run faster. However, 
-  # if you change any configuration or code from libraries loaded here, you'll
-  # need to restart spork for it take effect.
-  ENV["RAILS_ENV"] ||= 'test'
-  require File.expand_path("../../config/environment", __FILE__)
-  require 'rspec/rails'
 
-  # Requires supporting files with custom matchers and macros, etc,
-  # in ./support/ and its subdirectories.
+  require "rails/mongoid"
+  Spork.trap_class_method(Rails::Mongoid, :load_models)
+  require "rails/application"
+  Spork.trap_method(Rails::Application::RoutesReloader, :reload!)
+
+  require File.dirname(__FILE__) + "/../config/environment.rb" # <- see this.  Your hackery goes above this.  After this line is too late.
+  ENV["RAILS_ENV"] ||= 'test'
+  require 'rspec/rails'
+  require 'capybara/rspec'
+
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
   RSpec.configure do |config|
@@ -22,85 +27,17 @@ Spork.prefork do
     # config.mock_with :flexmock
     # config.mock_with :rr
     config.mock_with :rspec
-
-    #config.fixture_path = "#{::Rails.root}/spec/fixtures"
+    config.treat_symbols_as_metadata_keys_with_true_values = true
+    config.filter_run :focus => true
+    config.run_all_when_everything_filtered = true
 
     # If you're not using ActiveRecord, or you'd prefer not to run each of your
-    # examples within a transaction, comment the following line or assign false
+    # examples within a transaction, remove the following line or assign false
     # instead of true.
     #config.use_transactional_fixtures = true
   end
 end
 
 Spork.each_run do
-  # This code will be run each time you run your specs.
-  
+  FactoryGirl.reload
 end
-
-# --- Instructions ---
-# - Sort through your spec_helper file. Place as much environment loading 
-#   code that you don't normally modify during development in the 
-#   Spork.prefork block.
-# - Place the rest under Spork.each_run block
-# - Any code that is left outside of the blocks will be ran during preforking
-#   and during each_run!
-# - These instructions should self-destruct in 10 seconds.  If they don't,
-#   feel free to delete them.
-#
-
-
-
-
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-
-RSpec.configure do |config|
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-  config.mock_with :rspec
-
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  # config.use_transactional_fixtures = true  
-
-  # Clean up the database
-  require 'database_cleaner'
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
-    DatabaseCleaner.orm = "mongoid"
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.clean
-  end
-
-end
-
-# Clean the database after tests (MongoID only)
-# Uncomment to preserve DB through tests
-#config.before(:suite) do
-#  DatabaseCleaner[:mongoid].strategy = :truncation
-#end
-#
-#config.before(:each) do
-#  DatabaseCleaner[:mongoid].start
-#end
-#
-#config.after(:each) do
-#  DatabaseCleaner[:mongoid].clean
-#end

426e92903d3580f4602757541563d4dad10b82e0
Matt McCormick <matt.c.mccormick@gmail.com>
2011-09-30 11:29:06 -0400

    Fixed btn-tog s
    Signed-off-by: Matt McCormick <matt.c.mccormick@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,12 +31,22 @@ class UsersController < ApplicationController
   # Includes core objects mentioning this user
   def feed
     @user = User.find_by_slug(params[:id])
+    page = params[:p] ? params[:p].to_i : 1
+    @more_path = user_feed_path :p => page + 1
     @core_objects = CoreObject.feed(session[:feed_filters][:display], [:created_at, :desc], {
             :created_by_users => @user.following_users,
             :reposted_by_users => @user.following_users,
             :mentions_topics => @user.following_topics,
             :mentions_users => [@user.id]
     })
+    respond_to do |format|
+      if request.xhr?
+        html =  render_to_string :partial => "core_objects/feed", :locals => { :more_path => @more_path }
+        format.json { render json: { :event => "loaded_feed_page", :content => html } }
+      else
+        format.html
+      end
+    end
   end
 
   def contributions

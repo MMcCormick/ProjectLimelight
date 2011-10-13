0147a27b9ba9b35fb76719ee77cda9983542bd47
Marc MacLeod <marbemac@gmail.com>
2011-10-13 13:54:47 -0400

    couple more fixes / cleanup
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- app/jobs/sm_user_follow_user.rb
+++ app/jobs/sm_user_follow_user.rb
@@ -3,13 +3,12 @@ require 'json'
 #TODO: We need to update the soulmate data when relevant user data changes (username, main image, etc)
 class SmUserFollowUser
   include Resque::Plugins::UniqueJob
-  include Rails.application.routes.url_helpers
-  include ImageHelper
+  include SoulmateHelper
 
   @queue = :soulmate_user
 
   def initialize(user, following)
-    Soulmate::Loader.new("#{user.id.to_s}").add(user_nugget(following))
+    Soulmate::Loader.new(user.id.to_s).add(user_nugget(following))
   end
 
   def self.perform(user_id, following_user_id)

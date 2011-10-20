4f8c8096f50ce16ba3086944c4cb611f87c806d3
Marc MacLeod <marbemac@gmail.com>
2011-10-20 11:03:13 -0400

    moved comment thread to cell
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

---
+++ app/cells/comment_cell.rb
@@ -0,0 +1,14 @@
+class CommentCell < Cell::Rails
+
+  include Devise::Controllers::Helpers
+
+  def thread(talk_id, with_forms=false)
+    @talk_id = talk_id
+    @with_forms = with_forms
+    @comments = Comment.threaded_with_field(talk_id)
+    @user = current_user
+
+    render
+  end
+
+end

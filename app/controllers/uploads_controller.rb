b3b21ecd929688d4b93a2eb8bed6d29efcc7e95f
Marc MacLeod <marbemac@gmail.com>
2011-10-30 01:09:08 -0400

    bug
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -2,9 +2,9 @@ class UploadsController < ApplicationController
   def create
     name = "#{Digest::MD5.hexdigest(current_user.email.downcase)}-#{params[:file].original_filename}"
     directory = "#{Rails.root}/public/uploads"
-    Dir.mkdirs(directory) unless File.directory?(directory)
+    Dir.mkdir(directory) unless File.directory?(directory)
     directory = "#{Rails.root}/public/uploads/tmp"
-    Dir.mkdirs(directory) unless File.directory?(directory)
+    Dir.mkdir(directory) unless File.directory?(directory)
     path = File.join(directory, name)
     File.open(path, "wb") { |f| f.write(params[:file].read) }
 

8e3f69ef96054d47be5ecf3608b2cfb584a5e93c
Marc MacLeod <marbemac@gmail.com>
2011-10-29 22:57:36 -0400

    possible image upload fixes
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,7 +1,11 @@
 class UploadsController < ApplicationController
   def create
-    uploader = ImageUploader.new
-    uploader.cache!(params[:file])
-    render :json => {:image_location => uploader.cache_name, :image_path => uploader.current_path}
+    hash = "#{Digest::MD5.hexdigest(current_user.email.downcase)}-#{params[:file].original_filename}"
+    location = "#{Rails.root}/public/uploads/tmp/#{hash}"
+    writeOut = open(location, "wb")
+    writeOut.write(params[:file].read)
+    writeOut.close
+
+    render :json => {:image_location => hash, :image_path => location}
   end
 end

f7c943b934cd079f32a6d7eea729f96bb6b31a2c
Marc MacLeod <marbemac@gmail.com>
2011-11-26 14:18:12 -0500

    major style tweaks for cross browser. some fixes.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,9 +4,7 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-//= require jquery1.6.4
 //= require jquery_ujs
-//= require modernizr
 //= require json2
 //= require jquery.livequery
 //= require preload

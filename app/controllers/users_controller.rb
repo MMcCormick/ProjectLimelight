c139299e92d720969e14cce115135165699ba4df
Marc MacLeod <marbemac@gmail.com>
2011-11-30 20:14:22 -0500

    remove user caching

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!, :only => [:settings, :update, :picture_update, :update_settings]
   include ImageHelper
 
-  caches_action :default_picture, :cache_path => Proc.new { |c| "#{c.params[:id]}-#{c.params[:w]}-#{c.params[:h]}-#{c.params[:m]}" }
+  #caches_action :default_picture, :cache_path => Proc.new { |c| "#{c.params[:id]}-#{c.params[:w]}-#{c.params[:h]}-#{c.params[:m]}" }
 
   def show
     @user = User.find_by_slug(params[:id])

46717d9fc2bd5597dc440fd9afc00e01036322c2
Marc MacLeod <marbemac@gmail.com>
2011-12-15 14:32:26 -0500

    image fix
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -2,7 +2,15 @@ class UploadsController < ApplicationController
   def create
     uploader = ImageUploader.new
     uploader.store!(params[:file])
-    target_directory = Rails.env.development? ? 'http://localhost:3000/uploads/images/': 'http://staging.img.p-li.me/'
+
+    if Rails.env.development?
+      target_directory = 'http://localhost:3000/uploads/images/'
+    elsif Rails.env.staging?
+      target_directory = 'http://staging.img.p-li.me/'
+    else
+      target_directory = 'http://img.p-li.me/'
+    end
+
     render :json => {:image_location => "#{target_directory}tmp/#{uploader.filename}", :image_path => uploader.current_path}
   end
 end

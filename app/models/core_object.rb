2860967ab86dbef8282a1e736c69d795031bc9f0
Marc MacLeod <marbemac@gmail.com>
2011-12-14 19:25:17 -0500

    refactor list view
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- app/models/core_object.rb
+++ app/models/core_object.rb
@@ -89,7 +89,8 @@ class CoreObject
   def expire_caches
     ['list', 'grid', 'column'].each do |view|
       ActionController::Base.new.expire_fragment("teaser-#{id.to_s}-#{view}")
-      ActionController::Base.new.expire_fragment("teaser-#{id.to_s}-#{view}-response") # talk list view includes response on feeds but not on show pages.
+      ActionController::Base.new.expire_fragment("teaser-#{id.to_s}-#{view}-top-response") # talk list view includes response on feeds but not on show pages.
+      ActionController::Base.new.expire_fragment("teaser-#{id.to_s}-#{view}-bottom-response") # talk list view includes response on feeds but not on show pages.
       ActionController::Base.new.expire_fragment("teaser-#{id.to_s}-#{view}-top")
       ActionController::Base.new.expire_fragment("teaser-#{id.to_s}-#{view}-bottom")
     end

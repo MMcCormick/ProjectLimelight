68722e51f7f2db836451892b2b986c589c6813d0
Marc MacLeod <marbemac@gmail.com>
2011-12-28 16:06:29 -0500

    updated styling on interest section of user sidebars
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -5,10 +5,10 @@ module TopicsHelper
   end
 
   def topic_default_picture(topic, width, height, mode = 'fit')
-    if topic.fb_img == true
-      "https://usercontent.googleapis.com/freebase/v1/image#{topic.fb_id}?maxwidth=#{width}&maxheight=#{height}&mode=#{mode}"
-    else
-      topic_default_picture_path(topic, :w => width, :h => height, :m => mode)
-    end
+    #if topic.fb_img == true
+    #  "https://usercontent.googleapis.com/freebase/v1/image#{topic.fb_id}?maxwidth=#{width}&maxheight=#{height}&mode=#{mode}"
+    #else
+    topic_default_picture_path(topic, :w => width, :h => height, :m => mode)
+    #end
   end
 end

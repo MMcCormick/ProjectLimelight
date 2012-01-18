b41f9f930981c0066126b6d32689c990542f078c
Marc MacLeod <marbemac@gmail.com>
2012-01-18 14:49:22 -0500

    update splash page styling
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -203,7 +203,7 @@ class UsersController < ApplicationController
       @title = 'Welcome to Limelight!'
       @description = "The Limelight splash page, where users are directed to sign in"
       @show = params[:show] ? params[:show].to_sym : false
-      @topics = Topic.where(:health_index.gte => 2).order_by([[:pt, :desc]]).limit(750).to_a
+      @topics = Topic.where(:health_index.gte => 2).order_by([[:pt, :desc]]).limit(450).to_a
       @topics.shuffle!
 
       render "splash", :layout => "blank"

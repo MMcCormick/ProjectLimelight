e13dc8ae0c86fb54797cac7ddb831b714c6f90e9
Marc MacLeod <marbemac@gmail.com>
2012-01-19 21:45:54 -0500

    debug
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -162,10 +162,13 @@ $(function() {
         current_tile_group = $(".tile:eq("+chosen+")").data('group');
       }
 
+      console.log(last_tile_group);
+      console.log(current_tile_group);
+
       $('.'+last_tile_group).fadeTo(1000, .1, function() {
-        $(this).removeClass('hover');
+        $('.'+last_tile_group).removeClass('hover');
         $('.'+current_tile_group).fadeTo(1000, 1, function() {
-          $(this).addClass('hover');
+          $('.'+current_tile_group).addClass('hover');
         })
       })
 

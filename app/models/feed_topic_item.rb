da3f75cbfe1281899b87c8bc81f7fe0e9f5770c9
Marc MacLeod <marbemac@gmail.com>
2012-01-28 13:57:02 -0500

    started comment refactor
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- app/models/feed_topic_item.rb
+++ app/models/feed_topic_item.rb
@@ -14,19 +14,24 @@ class FeedTopicItem
       topic_mention_ids = post.topic_mentions.map{|tm| tm.id}
 
       # warning: do not use post.is_root? in this function, since topic roots are excluded
-      root_id = post.root_type == 'Topic' ? post.id : post.root_id
-      root_type = post.root_type == 'Topic' ? 'Talk' : post.root_type
+      if post.class.name == 'Talk' && post.root_type != 'Topic'
+        root_id = post.root_id
+        root_type = post.root_type
+      else
+        root_id = post.id
+        root_type = post.class.name
+      end
 
       latest_responses
 
-      # TODO: need positional operator $ to update latest_responses in place
-      # TODO: but $ can't be used with upsert, so: need to refactor into separate queries for root objs and responses
       updates = {"$set" => { :last_response_time => Time.now }}
       if root_id == post.id
         updates["$set"] = { :root_type => root_type, :root_mentions => topic_mention_ids }
       else
         updates["$addToSet"] = { :latest_responses => 'foobar' }
       end
+
+
 
       FeedTopicItem.collection.update({:root_id => root_id}, updates, {:upsert => true})
     end

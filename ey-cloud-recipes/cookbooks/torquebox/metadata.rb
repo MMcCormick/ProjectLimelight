788064390ca755ea782473c8eaa575970d2c9bf9
Marc MacLeod <marbemac@gmail.com>
2012-02-06 14:55:50 -0500

    chef recipes
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

---
+++ ey-cloud-recipes/cookbooks/torquebox/metadata.rb
@@ -0,0 +1,13 @@
+maintainer       "Jorge Falcão"
+maintainer_email "jlbfalcao@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures torquebox"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+depends "java"
+depends "install_from"
+
+supports "ubuntu"
+supports "debian"
+

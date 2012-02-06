2f07fa88c706541b7647236b54ad34a1ad134dfb
Marc MacLeod <marbemac@gmail.com>
2012-02-06 13:49:39 -0500

    setup production
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,9 @@ ProjectLimelight::Application.configure do
   # Use a different cache store in development
   #config.cache_store = :torque_box_store
 
+  # Use a different cache store in development
+  config.cache_store = :torque_box_store
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   #config.active_record.auto_explain_threshold_in_seconds = 0.5

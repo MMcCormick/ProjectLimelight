84ce7b2b85f6066da7d40140bdca4fe439e37c6a
Marc MacLeod <marbemac@gmail.com>
2012-02-07 14:15:20 -0500

    FIX TORQUEBOX YEEHAW
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- Procfile
+++
@@ -1,4 +0,0 @@
-web:           bundle exec unicorn -p $PORT -c ./config/unicorn.rb
-worker:        bundle exec rake resque:work QUEUE=neo4j,feeds,popularity,soulmate,images,notifications,slow
-scheduler:     bundle exec rake resque:scheduler
-neo4j_worker:  bundle exec rake resque:work QUEUE=neo4j
--- app/jobs/recalculate_clout.rb
+++ app/jobs/recalculate_clout.rb
@@ -1,15 +1,11 @@
-#class RecalculateClout
-#  include Resque::Plugins::UniqueJob
-#
-#  @queue = :popularity
-#
-#  def self.perform
-#    users = User.all.asc(:score)
-#    num_users = User.count
-#
-#    users.each_with_index do |user, i|
-#      user.clout = 2.5 * (i+1) / num_users + 0.5
-#      user.save!
-#    end
-#  end
-#end
+class RecalculateClout
+  def run
+    users = User.all.asc(:score)
+    num_users = User.count
+
+    users.each_with_index do |user, i|
+      user.clout = 2.5 * (i+1) / num_users + 0.5
+      user.save!
+    end
+  end
+end

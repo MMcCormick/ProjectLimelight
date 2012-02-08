160a557660c575b701a33d73d3e86c369c57a54c
Marc MacLeod <marbemac@gmail.com>
2012-02-08 14:29:39 -0500

    quick
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,3 @@
-Airbrake.configure do |config|
-  config.api_key = 'abbffbe692824d915f9543d6cf2321ec'
-end
+#Airbrake.configure do |config|
+#  config.api_key = 'abbffbe692824d915f9543d6cf2321ec'
+#end

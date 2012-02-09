1699ad82c9f68ebfa77aac49c9f490762106033e
Marc MacLeod <marbemac@gmail.com>
2012-02-08 19:42:08 -0500

    undo
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- app/jobs/push_like.rb
+++ app/jobs/push_like.rb
@@ -5,6 +5,6 @@ class PushLike
   def self.perform(object_id, user_id)
     object = CoreObject.find(object_id)
     user = User.find(user_id)
-    object.push_like(user) if object && user    d
+    object.push_like(user) if object && user
   end
 end

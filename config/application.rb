7da8ed15f10c8a41c3b79ef4e82216570cc7d10d
Marc MacLeod <marbemac@gmail.com>
2012-06-15 15:11:13 -0400

    expire assets
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -71,7 +71,7 @@ module ProjectLimelight
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '.1'
+    config.assets.version = '.2'
 
     config.assets.initialize_on_precompile = false
   end

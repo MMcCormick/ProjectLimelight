95e310613c7dc7d9bd0ccd4b16106fff2483065e
Marc MacLeod <marbemac@gmail.com>
2012-07-09 20:25:51 -0400

    test job script
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- app/controllers/testing_controller.rb
+++ app/controllers/testing_controller.rb
@@ -5,15 +5,7 @@ class TestingController < ApplicationController
   def test
     authorize! :manage, :all
 
-    #Resque.enqueue(TestJob)
-
-    Notification.each do |n|
-      if [:repost, :comment, :also].include?(n.type.to_sym)
-        unless n.object
-          n.destroy
-        end
-      end
-    end
+    Resque.enqueue(TestJob)
 
   end
 
--- app/jobs/test_job.rb
+++ app/jobs/test_job.rb
@@ -4,10 +4,22 @@ class TestJob
 
   def self.perform()
 
+    Post.all.each do |p|
+      unless p.post_media_id && p.post_media
+        p.destroy
+      end
+    end
+
     User.all.each do |u|
       u.topic_activity_recalculate
       u.topic_likes_recalculate
       u.save
+    end
+
+    Topic.all.each do |t|
+      users = User.where(:following_topics => t.id)
+      t.followers_count = users.length
+      t.save
     end
 
   end

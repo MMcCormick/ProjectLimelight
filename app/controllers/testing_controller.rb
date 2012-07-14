6001b619eb523add628506e981f09f07812c61b5
Marc MacLeod <marbemac@gmail.com>
2012-07-14 19:11:28 -0400

    fix
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- app/controllers/testing_controller.rb
+++ app/controllers/testing_controller.rb
@@ -8,7 +8,7 @@ class TestingController < ApplicationController
     #@count1 = 0
     #@count2 = 0
 
-    #Resque.enqueue(TestJob)
+    Resque.enqueue(TestJob)
 
     #data = [
     #  [[2,3,4],[2,3,5]],
@@ -19,42 +19,6 @@ class TestingController < ApplicationController
     #  [[7,6,5],[7,6,4],[7,10]],
     #  [10]
     #]
-
-    PostMedia.each do |pm|
-      pm.shares = []
-      pm.ll_score = 0
-      pm.comments = []
-      pm.comment_count = 0
-      pm.save
-    end
-
-    Post.all.each do |p|
-      media = p.post_media
-
-      unless media
-        p.destroy
-        next
-      end
-
-      media.add_share(p.user_id, p.content, p.topic_mention_ids, [], {:limelight => nil})
-
-      p.comments.each do |c|
-        media.add_comment(c.user_id, c.content)
-      end
-      media.save
-    end
-
-    PostMedia.each do |pm|
-      pm.shares.each do |s|
-        FeedUserItem.push_post_through_users(pm, s.user, false, true)
-        FeedUserItem.push_post_through_topics(pm)
-      end
-    end
-
-    #post = PostMedia.first
-    #post.shares.each do |s|
-    #  FeedUserItem.push_post_through_users(post, s.user, false, true)
-    #end
 
   end
 

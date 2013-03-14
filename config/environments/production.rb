d5473afdb944a36a4898b1807027da79cbd632d0
Marc MacLeod <marbemac@gmail.com>
2013-03-14 18:38:43 -0400

    debug
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,15 +12,15 @@ ProjectLimelight::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = false
+  config.assets.compress = faltruese
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
   # Generate digests for assets URLs
-  config.assets.digest = false
+  config.assets.digest = true
 
-  config.assets.debug = true
+  #config.assets.debug = true
 
   #config.static_cache_control = "public, max-age=2592000"
 

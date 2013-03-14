4d31d1fd7e7dbff2fd98b8f1a40122a930be410e
Marc MacLeod <marbemac@gmail.com>
2013-03-14 13:38:01 -0400

    memcached
    Signed-off-by: Marc MacLeod <marbemac@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,13 +37,7 @@ ProjectLimelight::Application.configure do
   # config.logger = SyslogLogger.new
 
   # Use a different cache store in production
-  config.cache_store = :dalli_store
-
-  config.action_dispatch.rack_cache = {
-    :metastore    => Dalli::Client.new,
-    :entitystore  => 'file:tmp/cache/rack/body',
-    :allow_reload => false
-  }
+  config.cache_store = :dalli_store, 'localhost:11211'
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
